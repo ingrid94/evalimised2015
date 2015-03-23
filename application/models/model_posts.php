10bc0bf5f2079faad8c97a3e4d8fb01ba015eeb3
Ingrid Kaasik <ingrid.kaasik@eesti.ee>
2015-03-23 07:02:17 +0200

    Forgot the model file.

---
+++ application/models/model_posts.php
@@ -0,0 +1,14 @@
+<?php
+class model_posts extends CI_Model {
+
+	public function getPosts(){
+		$sql = "SELECT `Candidate`.`Id`AS CandId, `Fraction`.`Name` AS FraNa,`Users`.`LastName` AS LName, `Users`.`Forename`AS FName, `Candidate`.`Description` AS Descript\n"
+    		. "	FROM `Users`\n"
+    		. "	JOIN `ev2015`.`Candidate` ON `Users`.`Id` = `Candidate`.`U_Id`\n"
+    		. "	JOIN `ev2015`.`Fraction` ON `Candidate`.`Fr_Id`= `Fraction`.`Id`\n"
+    		. "	ORDER BY `Candidate`.`Id` ASC";
+		$query = $this->db->query($sql);
+		return $query->result();
+	}
+}
+?>
