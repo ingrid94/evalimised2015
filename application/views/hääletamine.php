bb3dba0765d08206d3ef995f1a0830b29a036b49
ingrid94 <ingrid.kaasik@eesti.ee>
2015-05-05 08:43:20 +0300

    Lisasin hääletamise nupu, mis hetkel ei tööta

--- application/views/hääletamine.php
+++ application/views/hääletamine.php
@@ -1,29 +1,35 @@
 <div id="hääletamine" class="sisu">
 	<h1>Hääletamine</h1>
 	<p>Hääletamiseks märgi sobiva kandidaadi ees ring</p>
-	<table id="haaletamine_tabel" class="display" cellspacing="0" width="100%">
+	<table id="haaletamine_tabel" class="table">
 		<thead>
-      	<tr>
-			<th>Vali!</th>
-        	<th>Kandidaadi number</th>
-        	<th>Partei nimi</th>
-       		<th>Kandidaadi perenimi</th>
-			<th>Kandidaadi eesnimi</th>
-			<th>Kandidaadi kirjeldus</th>
-      	</tr>
-   	</thead>
+      		<tr>
+				<th>Vali!</th>
+        		<th>Kandidaadi number</th>
+        		<th>Partei nimi</th>
+       			<th>Kandidaadi perenimi</th>
+				<th>Kandidaadi eesnimi</th>
+				<th>Kandidaadi kirjeldus</th>
+      		</tr>
+   		</thead>
 
-	<tbody><form>
-		<?php foreach($region as $row){?>
-			<tr>
-				<td><input type="radio" name="vali" value="<?php echo $row->CandId;?>"></td>
-				<td><?php echo $row->CandId;?></td>
-				<td><?php echo $row->FraNa;?></td>
-				<td><?php echo $row->LName;?></td>
-				<td><?php echo $row->FName;?></td>
-				<td><?php echo $row->Descript;?></td>
-			</tr>     
-		<?php }?>
-	</form></tbody>
-</table>
+		<tbody>
+			<form>
+			<?php foreach($region as $row){?>
+				<tr>
+					<td><input type="radio" name="vali" value="<?php echo $row->CandId;?>"></td>
+					<td><?php echo $row->CandId;?></td>
+					<td><?php echo $row->FraNa;?></td>
+					<td><?php echo $row->LName;?></td>
+					<td><?php echo $row->FName;?></td>
+					<td><?php echo $row->Descript;?></td>
+				</tr>     
+			<?php }?>
+			</form>
+		</tbody>
+	</table>
+	<div class="btn-group" role="group" aria-label="...">
+  		<a href="http://ev2015.cs.ut.ee/index.php/site/nimekiri" class="btn btn-default" role="button">Hääleta!</a>
+	</div>
 </div>
+
